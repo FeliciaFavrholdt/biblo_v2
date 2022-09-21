fef08d14128f7903d61c578746bb2e0e3687b85e
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2022-09-21 14:24:58 +0200

    adds data fields + constructor + getters/setters

--- src/Entity/Book.java
+++ src/Entity/Book.java
@@ -1,4 +1,38 @@
 package Entity;
 
 public class Book {
+
+    private int bookID;
+    private String title;
+    private String author;
+
+    public Book(int bookID, String title, String author) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
