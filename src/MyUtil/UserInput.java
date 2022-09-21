36d83bae1c85ca729a81985b90939dcca9e1e751
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2022-09-21 14:11:10 +0200

    adds MyUtil package and getString method

---
+++ src/MyUtil/UserInput.java
@@ -0,0 +1,12 @@
+package MyUtil;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+    public static String getString(String s) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(s + " : ");
+        return scanner.nextLine();
+    }
+}
