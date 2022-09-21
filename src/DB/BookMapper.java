16a66663df8670598b4748bed3c53b7d9505d449
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2022-09-21 14:41:27 +0200

    adds method Book addBook()

--- src/DB/BookMapper.java
+++ src/DB/BookMapper.java
@@ -1,4 +1,34 @@
 package DB;
 
+import Entity.Book;
+
+import java.sql.*;
+
 public class BookMapper {
+
+    public static Book addBook(Book book) {
+
+        String sql = "INSERT INTO Bog (Forfatter , Title ) VALUES (?, ?)";
+
+        try (Connection con = ConnectionConfiguration.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+
+            try {
+                ps.setString(1, book.getAuthor());
+                ps.setString(2, book.getTitle());
+                ps.executeUpdate();
+
+                ResultSet resultSet = ps.getGeneratedKeys();
+                resultSet.next();
+
+                int bookId = resultSet.getInt(1);
+                book.setBookId(bookId);
+
+                return book;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return book;
+        }
+    }
 }
--- src/DB/Facade.java
+++ src/DB/Facade.java
@@ -1,4 +1,12 @@
 package DB;
 
+import Entity.Book;
+
 public class Facade {
+
+    public static Book addBook(Book book) {
+        return BookMapper.addBook(book);
+    }
+
+
 }
