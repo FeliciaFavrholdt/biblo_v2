21fa5d93097e2e69ac66b15cf1c72553fe83e19c
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2022-09-21 20:02:53 +0200

    adds Customers and BookRental methods to Facade.java

--- src/DB/Facade.java
+++ src/DB/Facade.java
@@ -1,16 +1,15 @@
 package DB;
 
 import Entity.Book;
-
 import java.sql.SQLException;
 import java.util.List;
 
 public class Facade {
 
+    //BOOKS
     public static Book addBook(Book book) throws SQLException {
         return BookMapper.addBook(book);
     }
-
 
     public static List<Book> fetchBooks() {
         return BookMapper.fetchBooks();
@@ -18,5 +17,31 @@ public class Facade {
 
     public static String deleteBook(int bookID) {
         return BookMapper.deleteBook(bookID);
+    }
+
+    //CUSTOMERS
+    public static Customer addCustomer(Customer customer) throws SQLException {
+        return CustomerMapper.addCustomer(customer);
+    }
+
+    public static List<Customer> fetchCustomers() {
+        return CustomerMapper.fetchCustomers();
+    }
+
+    public static String deleteCustomer(int CustomerID) {
+        return CustomerMapper.deleteCustomer(customerID);
+    }
+
+    //BOOK RENTALS
+    public static BookRental addBookRental(BookRental bookRental) throws SQLException {
+        return BookRentalMapper.addBookRental(bookRental);
+    }
+
+    public static List<BookRental> fetchBookRentals() {
+        return BookRentalMapper.fetchBookRentals();
+    }
+
+    public static String deleteBookRental(int bookRentalID) {
+        return BookRentalMapper.deleteBookRental(bookRentalID);
     }
 }
